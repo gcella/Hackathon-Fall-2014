64a30878d28937d273d85fa149d38b3be4078b24
George Brown <gbrown3593@gmail.com>
2014-10-24 21:47:16 -0400

    scraping...

--- web.js
+++ web.js
@@ -10,6 +10,23 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
+app.get('/getEvents', function(req, res) {
+	var url = "http://events.tufts.edu/static";
+	request(url, function(error, response, html) {
+		if(!error) {
+			var $ = cheerio.load(html);
+			var date, eventn, campus;
+			var json = { date : "", eventn : "", campus : ""};
+
+			$('.static-blog-descr').filter(function() {
+				var data = $(this);
+
+				json.data = 
+			})
+		}
+	})
+})
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);

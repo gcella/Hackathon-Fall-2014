a1f1755637d3562fc5b7ac668e30332326d12d14
McCall Bliss <missmccallkay@gmail.com>
2014-10-25 02:23:56 -0400

    update home page styles and backbone retrieval

--- app/js/views/EventView.js
+++ app/js/views/EventView.js
@@ -8,6 +8,7 @@ define(['models/EventModel'], function(EventModel){
         this.model.on('change', this.render());
     },
     render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
         return this;
     }, 
     events: {

8f9473ca9ea23255cc5f992a5b15b199f27966be
George Brown <gbrown3593@gmail.com>
2014-10-25 07:31:35 -0400

    starting db

--- web.js
+++ web.js
@@ -3,13 +3,15 @@ var app = express();
 var util = require('util');
 var request = require('request');
 var cheerio = require('cheerio');
-var pg = require('pg');
-var conString = "postgres://ihihinqvcgnpgv:AInbBQ7QFjQWlpGB3XNyHCEktb@ec2-54-225-136-187.compute-1.amazonaws.com/dqraht2o37lsn";
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
+var mongo = require("mongodb");
+//var MongoClient = require('mongod').MongoClient;
+//var MONGOLAB_URI = "mongodb://heroku_app30984285:asrdlrc74fb18cvqal0d8r0gtq@ds047950.mongolab.com:47950/heroku_app30984285";
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
+        "mongodb://localhost/scorecenter";
+
+var db = mongo.Db.connect(mongoUri, function (err, database) {
+      db = database;
 });
 
 //for security, we should implement this (later) <-- LOL
@@ -24,57 +26,55 @@ app.get('/', function(req, res){
 });
 
 app.get('/test', function(req, res){
-  res.sendfile('matts.json');
+  //res.sendfile('matts.json');
   //res.send("hello!");
 });
 
 app.get('/clubs', function(req, res) {
-  client.query('SELECT * FROM tuftsclubs.groups;', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    res.json(result.rows[0]);
-  });
+	res.send("fuck.");
 });
 
 app.get("/clubs/:id", function(req, res) {
-    client.query('SELECT * FROM tuftsclubs.groups WHERE name = ' + req.params.id + ';', function(err, result) {
-        if(err) {
-            return console.error('error running query', err);
-        }
-    res.json(result.rows);
-  });
+
 });
 
-app.get('/events', function(req, res) {
+app.get('/tuftsevents', function(req, res) {
 	var url = "http://events.tufts.edu/static";
 	request(url, function(error, response, html) {
 		if(!error) {
 			var $ = cheerio.load(html);
 
-			var json = { "dates": [], "events": [], "descrs": [] };
+			//var json = { "dates": [], "events": [], "descrs": [] };
 
 			$('.static-blog-descr').text(function( i ) {
 				var data = $(this);
 
-				json.events[i] = data.children().first().text();
-
+				//json.events[i] = data.children().first().text();
+				var events = JSON.stringify(data.children().first().text());
 				var dataString = data.toString();
 				var save = dataString.substr(dataString.indexOf('</h2>') + 5, dataString.length);
 				var newString = dataString.substr(dataString.indexOf('</h2>') + 5, dataString.length);
 
 				var dateString = newString.substr(0, newString.indexOf('<br>'));
-				json.dates[i] = dateString; 
+				//json.dates[i] = dateString; 
+				dateString = JSON.stringify(dateString);
 
 				description = save.substr(save.indexOf('<br>'), save.indexOf('<b>')).split('<b>');
-				json.descrs[i] = description;
+				description = JSON.stringify(description);
+
+				db.collection("events", function(er, collection) {
+				if(events && dateString && description) {
+					var currTime = new Date().toUTCString();
+					collection.insert({"name":events, "description": description, "eventdate": dateString, "created_at": currTime}, function(err,r){});
+				} else {
+					console.log("bad data entry error");
+				}
 			});
-
-			var x = JSON.stringify(json, null, 4);
-			res.send(x);
-
-		}
-	});
+			//var x = JSON.stringify(json, null, 4);
+			res.send("hello");
+		});
+}
+});
 });
 
 app.get('/addevent', function(req, res) {
@@ -82,25 +82,16 @@ app.get('/addevent', function(req, res) {
 });
 
 app.post('/newevent', function(req, res) {
-	// pretending that i have all the shit here
-	// sets vars
-	var client = new pg.Client(conString);
-	client.connect(function(err) {
-	  if(err) {
-	    return console.error('could not connect to postgres', err);
-	  }
-	  client.query('INSERT INTO tuftsclubs.events VALUES( DEFAULT, ' + 
-	  	            name + ', ' + video + ', ' + description + ', ' +
-	  	            freefood + ', ' + 'SELECT id FROM tuftsclubs.groups WHERE name = ' +
-	  	            name + ', ' + "'{" + ph1 + ', ' + ph2 + ', ' + ph3 + ', ' + ph4 + "}', " +
-	  	            + date + ', ' + time + ');', function(err, result) {
-	    if(err) {
-	      return console.error('error running query', err);
-	    }
-	    console.log(result.rows[0]);
-	    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-	    client.end();
-	  });
+	db.collection("events", function(er, collection) {
+		if(req.body.evn && req.body.date && req.body.descr) {
+			var evenName = req.body.evn;
+			var evDate = req.body.date;
+			var descript = req.body.descr;
+			var currTime = new Date().toUTCString();
+			collection.insert({"name":evenName, "description": descript, "eventdate": evDate, "created_at": currTime}, function(err,r){});
+		} else {
+			console.log("bad data entry error");
+		}
 	});
 });
 

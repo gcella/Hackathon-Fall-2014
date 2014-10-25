12c471f2690ae921224c2a994430eaa3ab6a1341
McCall Bliss <missmccallkay@gmail.com>
2014-10-25 04:20:28 -0400

    web.js clubs

--- app/js/models/EventModel.js
+++ app/js/models/EventModel.js
@@ -1,14 +1,15 @@
-define([], function() {
+define(['moment'], function(moment) {
   var EventModel = Backbone.Model.extend({
     defaults: {
         name: "Event", 
         date: 01012015,
+        start_time: new moment(),
+        location: "",
         id: "",
         photos: {}, 
         videoUrl: "", 
         description: "",
         freeFood: false,
-        location: "",
         club: "",
         isEvent: false
     }, 
--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var app = express();
 var util = require('util');
 var request = require('request');
 var cheerio = require('cheerio');
+var pg = require('pg');
+var conString = "postgres://ihihinqvcgnpgv:AInbBQ7QFjQWlpGB3XNyHCEktb@ec2-54-225-136-187.compute-1.amazonaws.com/dqraht2o37lsn";
+
 //for security, we should implement this (later) <-- LOL
 //var escape = require('escape');
 
@@ -17,6 +20,22 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){
   res.sendfile('matts.json');
 });
+
+app.get('/clubs', function(req, res) {
+    pg.connect(conString, function(err, client, done) {
+      if(err) {
+        res.send('Error fetching data');
+      }
+      client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+        if(err) {
+          return console.error('error running query', err);
+        }
+        res.send(result);
+      });
+    });
+})
 
 app.get('/events', function(req, res) {
 	var url = "http://events.tufts.edu/static";
